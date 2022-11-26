43d27a20fd4c0f0f26ee5696a48646e34b787e8f
Ehsan Akbar <45014174+akbar-ehsaan@users.noreply.github.com>
2022-11-26 16:24:08 +0330

    Add files via upload

---
+++ Akka.netActorBehavior/Messages.cs
@@ -0,0 +1,86 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.netActorBehavior
+{
+    public class PlaySongMessage
+
+    {
+
+        public PlaySongMessage(string song)
+
+        {
+
+            Song = song;
+
+        }
+
+        public string Song { get; }
+
+    }
+
+    public class StopPlayingMessage
+
+    {
+
+    }
+    public class MusicPlayerActor : ReceiveActor
+    {
+        protected string CurrentSong;
+        public MusicPlayerActor()
+        {
+            StoppedBehavior();
+        }
+        private void StoppedBehavior()
+
+        {
+
+            Receive<PlaySongMessage>(m => PlaySong(m.Song));
+
+            Receive<StopPlayingMessage>(m =>
+
+                           Console.WriteLine("Cannot stop, the actor is already stopped"));
+
+        }
+        private void PlayingBehavior()
+
+        {
+
+            Receive<PlaySongMessage>(m =>
+
+                     Console.WriteLine($"Cannot play. Currently playing '{CurrentSong}'"));
+
+            Receive<StopPlayingMessage>(m => StopPlaying());
+
+        }
+
+        private void PlaySong(string song)
+
+        {
+
+            CurrentSong = song;
+
+            Console.WriteLine($"Currently playing '{CurrentSong}'");
+
+            Become(PlayingBehavior);
+
+        }
+
+        private void StopPlaying()
+
+        {
+
+            CurrentSong = null;
+
+            Console.WriteLine($"Player is currently stopped.");
+
+            Become(StoppedBehavior);
+
+        }
+
+    }
+}
---
+++ Akka.netActorBehavior/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Akka.Actor;
+using Akka.netActorBehavior;
+
+Console.WriteLine("Hello, World!");
+ActorSystem system = ActorSystem.Create("my-first-akka");
+
+IActorRef musicPlayer = system.ActorOf<MusicPlayerActor>("musicPlayer");
+
+musicPlayer.Tell(new PlaySongMessage("Smoke on the water"));
+
+musicPlayer.Tell(new PlaySongMessage("Another brick in the wall"));
+
+musicPlayer.Tell(new StopPlayingMessage());
+
+musicPlayer.Tell(new StopPlayingMessage());
+
+musicPlayer.Tell(new PlaySongMessage("Another brick in the wall"));
+
+Console.Read();
+
+system.Terminate();
